554ffeb442f5cedbd0ffb7d3c06b44d09a545506
Avanii0809 <63725367+Avanii0809@users.noreply.github.com>
2026-04-09 13:53:53 +0530

    UC8
    Refactored palindrome checking logic using a queue and stack for better clarity and efficiency.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,42 +1,34 @@
-import java.util.Scanner;
-public class PalindromeCheckerApp {
+package org.example;
+
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class PallindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Input text: ");
-        String input = sc.nextLine();
-        String reversed = "";
+
+        String input = "civic";
+
+        String processedInput = input.replaceAll("\\s+", "").toLowerCase();
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : processedInput.toCharArray()) {
+            queue.add(ch);     // FIFO
+            stack.push(ch);    // LIFO
+        }
+
         boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-        boolean isPalindrome1 = input.equals(reversed);
-        System.out.println("Is it a Palindrome? : " + isPalindrome1);
-
-        String input1 = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
-        boolean isPalindrome2 = true;
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome2 = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-        System.out.println("Input : " + input1);
-        System.out.println("Is Palindrome? : " + isPalindrome2);
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
